11033c0997e04366fa0fa4e7ed0e3606e7eb76d6
vaibhav <1vaibhavsrivastava@gmail.com>
2021-07-27 13:57:41 +0530

    Push

---
+++ Armstrong number/src/AN/number.java
@@ -0,0 +1,32 @@
+package AN;
+
+import java.util.Scanner;
+
+public class number 
+{
+	public static void main(String[] args) 
+	{
+		int n,
+		sum=0, num, r;
+		Scanner s= new Scanner(System.in);
+		System.out.println("please enter a number");
+		n = s.nextInt();
+		
+		num=n;
+		while(num>0)
+		{
+			r=num%10;
+			sum = sum+(r*r*r);
+			num=num/10;
+			
+		}
+		if(n==sum)
+		{
+			System.out.println("this is armstrong number");
+		}
+		else
+		{
+			System.out.println("this is not armstrong number");
+		}
+	}
+}
